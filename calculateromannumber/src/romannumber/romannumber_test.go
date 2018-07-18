beea7483dd43f04ba95fa18e88cdb90371c8b1a3
teerawat.th <teerawat.th@baac.or.th>
2018-07-18 16:41:01 +0700

    [Created] : ฟังก์ชัน convertToRomanNumber

--- calculateromannumber/src/romannumber/romannumber_test.go
+++ calculateromannumber/src/romannumber/romannumber_test.go
@@ -4,69 +4,69 @@ import (
 	"testing"
 )
 
-func Test_convertToRomanNumber_Input_Number_1_Should_Be_I(t *testing.T) {
+func Test_ConvertToRomanNumber_Input_Number_1_Should_Be_I(t *testing.T) {
 	number := 1
 	expectedRomanNumber := "I"
 
-	actualRomanNumber := convertToRomanNumber(number)
+	actualRomanNumber := ConvertToRomanNumber(number)
 	if expectedRomanNumber != actualRomanNumber {
 		t.Errorf("expected %s but got %s", expectedRomanNumber, actualRomanNumber)
 	}
 }
 
-func Test_convertToRomanNumber_Input_Number_2_Should_Be_II(t *testing.T) {
+func Test_ConvertToRomanNumber_Input_Number_2_Should_Be_II(t *testing.T) {
 	number := 2
 	expectedRomanNumber := "II"
 
-	actualRomanNumber := convertToRomanNumber(number)
+	actualRomanNumber := ConvertToRomanNumber(number)
 	if expectedRomanNumber != actualRomanNumber {
 		t.Errorf("expected %s but got %s", expectedRomanNumber, actualRomanNumber)
 	}
 }
-func Test_convertToRomanNumber_Input_Number_4_Should_Be_IV(t *testing.T) {
+func Test_ConvertToRomanNumber_Input_Number_4_Should_Be_IV(t *testing.T) {
 	number := 4
 	expectedRomanNumber := "IV"
 
-	actualRomanNumber := convertToRomanNumber(number)
+	actualRomanNumber := ConvertToRomanNumber(number)
 	if expectedRomanNumber != actualRomanNumber {
 		t.Errorf("expected %s but got %s", expectedRomanNumber, actualRomanNumber)
 	}
 }
-func Test_convertToRomanNumber_Input_Number_5_Should_Be_V(t *testing.T) {
+func Test_ConvertToRomanNumber_Input_Number_5_Should_Be_V(t *testing.T) {
 	number := 5
 	expectedRomanNumber := "V"
 
-	actualRomanNumber := convertToRomanNumber(number)
+	actualRomanNumber := ConvertToRomanNumber(number)
 	if expectedRomanNumber != actualRomanNumber {
 		t.Errorf("expected %s but got %s", expectedRomanNumber, actualRomanNumber)
 	}
 }
 
-func Test_convertToRomanNumber_Input_Number_6_Should_Be_VI(t *testing.T) {
+func Test_ConvertToRomanNumber_Input_Number_6_Should_Be_VI(t *testing.T) {
 	number := 6
 	expectedRomanNumber := "VI"
 
-	actualRomanNumber := convertToRomanNumber(number)
+	actualRomanNumber := ConvertToRomanNumber(number)
 	if expectedRomanNumber != actualRomanNumber {
 		t.Errorf("expected %s but got %s", expectedRomanNumber, actualRomanNumber)
 	}
 }
 
-func Test_convertToRomanNumber_Input_Number_9_Should_Be_IX(t *testing.T) {
+func Test_ConvertToRomanNumberr_Input_Number_9_Should_Be_IX(t *testing.T) {
 	number := 9
 	expectedRomanNumber := "IX"
 
-	actualRomanNumber := convertToRomanNumber(number)
+	actualRomanNumber := ConvertToRomanNumber(number)
 	if expectedRomanNumber != actualRomanNumber {
 		t.Errorf("expected %s but got %s", expectedRomanNumber, actualRomanNumber)
 	}
 }
 
-func Test_convertToRomanNumber_Input_Number_10_Should_Be_X(t *testing.T) {
+func Test_ConvertToRomanNumber_Input_Number_10_Should_Be_X(t *testing.T) {
 	number := 10
 	expectedRomanNumber := "X"
 
-	actualRomanNumber := convertToRomanNumber(number)
+	actualRomanNumber := ConvertToRomanNumber(number)
 	if expectedRomanNumber != actualRomanNumber {
 		t.Errorf("expected %s but got %s", expectedRomanNumber, actualRomanNumber)
 	}

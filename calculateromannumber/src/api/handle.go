fbac8b4c44a7e35ce46ec8a70308920861b79ab2
teerawat.th <teerawat.th@baac.or.th>
2018-07-18 16:40:11 +0700

    [Created]: test api

--- calculateromannumber/src/api/handle.go
+++ calculateromannumber/src/api/handle.go
@@ -1,1 +1,24 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"romannumber"
+)
+
+type RomanResponse struct {
+	RomanNumber string `json:"romanNumber"`
+}
+
+func NumberToRomanNumberHandler(writer http.ResponseWriter, request *http.Request) {
+	numberString := request.URL.Query().Get("number")
+	number, _ := strconv.Atoi(numberString)
+
+	roman := romannumber.ConvertToRomanNumber(number)
+	romanJson, _ := json.Marshal(RomanResponse{
+		RomanNumber: roman,
+	})
+	writer.Write(romanJson)
+}
